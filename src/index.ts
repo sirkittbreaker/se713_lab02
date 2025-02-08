a6db817f059bb618edd062c5be118454caa7ef4a
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-02-08 13:45:14 +0700

    task 3, 5

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,12 @@ const add = (a: number, b: number): string => {
 
 const result = add(1, 2) + 0;
 console.log(result, "type of result: ", typeof result);
+
+const findMax = (numbers: number[]): string => {
+  const max = Math.max(...numbers);
+  return max.toString();
+};
+
+const numbers = [29, 11, 3, 55, 34];
+const maxNumber = findMax(numbers);
+console.log(maxNumber, "type of maxNumber: ", typeof maxNumber);
