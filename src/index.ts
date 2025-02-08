985cfa7d37df85a55017fd4b2fbbde9f6c357e89
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-02-08 13:28:11 +0700

    task 2, 3

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 let x: number | string = 10;
-x = "hello";
+x = 5;
+
+if (typeof x === "string") {
+  console.log("x is a string");
+} else if (typeof x === "number") {
+  console.log("x is a number");
+} else {
+  console.log("x is neither a string nor a number");
+}
