c814630ac837ca4668347a9667c4edbdc320a3ce
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-02-08 14:07:21 +0700

    task 4, 5

--- src/function.ts
+++ src/function.ts
@@ -3,6 +3,11 @@ export const add = (a: number, b: number): string => {
   return result.toString();
 };
 
+export const subtract = (a: number, b: number): string => {
+  const result = a - b;
+  return result.toString();
+};
+
 const findMax = (numbers: number[]): string => {
   const max = Math.max(...numbers);
   return max.toString();
