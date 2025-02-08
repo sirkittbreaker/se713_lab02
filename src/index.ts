8d0ec8f6a7285da1af41e909054423ae6479b21a
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-02-08 13:35:09 +0700

    task 3, 1

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-let x: number | string = 10;
-x = 5;
+const add = (a, b) => {
+  return a + b;
+};
 
-if (typeof x === "string") {
-  console.log("x is a string");
-} else if (typeof x === "number") {
-  console.log("x is a number");
-} else {
-  console.log("x is neither a string nor a number");
-}
+console.log(add(1, 2));
